dd4fa10de52f5949bed9f4aefefd5e176f247112
Станислав Маяцкий <stasm226@gmail.com>
2025-02-15 15:09:00 +0500

    Add redis as a cache broker

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,9 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {"BACKEND": "django.core.cache.backends.redis.RedisCache", "LOCATION": "redis://localhost:6379/1"}
+    }
